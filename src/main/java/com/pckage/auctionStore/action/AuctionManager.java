4661f3db436dcfce4a44afd51f53af072b9bb6ae
Acer <Acer@AS5745>
2018-06-18 22:15:55 +0530

    comments update

--- src/main/java/com/pckage/auctionStore/action/AuctionManager.java
+++ src/main/java/com/pckage/auctionStore/action/AuctionManager.java
@@ -9,21 +9,44 @@ import java.util.stream.Collectors;
 import com.pckage.auctionStore.core.Bid;
 import com.pckage.auctionStore.core.Item;
 import com.pckage.auctionStore.core.Listner;
-
+/**
+ * This class is responsible for creation of Object of AuctionManager.
+ * This class is also responsible for managing auctions.
+ * 
+ * @author Abhishek Singh
+ * @version 1.0
+ */
 public class AuctionManager {
+	/** 
+	 * To get the single instance of AuctionManager class
+	 * */
 	private static AuctionManager auctionManager;
+	/**
+	 * To store and manage bid for said item
+	 * */
 	private static ConcurrentHashMap<Item, List<Bid>> bidManager;
+	/**
+	 * Listner class to monitor bid time restrictions to get highest bid winner
+	 * */
 	private Listner listner=Listner.getInstance();
-
+	/**
+	 * Constructore of AuctionManager class responsible for starting listener and start auction.
+	 * */
 	private AuctionManager() {
 		listner.startListner();
 		initAuction();
 	}
-
+	/**
+	 * This function initiate auction by initialization of bid manager.
+	 * */
 	private void initAuction() {
 		bidManager = new ConcurrentHashMap<>();
 	}
-
+	/**
+	 * This function return the single instance of AuctionManager class
+	 * 
+	 * @return Object of AuctionManager class
+	 * */
 	public static AuctionManager getAuctionManager() {
 		if (auctionManager == null) {
 			synchronized (AuctionManager.class) {
@@ -34,8 +57,13 @@ public class AuctionManager {
 		}
 		return auctionManager;
 	}
-
-	public synchronized void addBid(Item item, Bid bid) {
+	/**
+	 * This function adds bid to bid manager, notify listener about the bid.
+	 * 
+	 * @param Item final object of item class
+	 * @param Bid object of Bid class 
+	 * */
+	public synchronized void addBid(final Item item, Bid bid) {
 		if (bidManager.get(item) == null) {
 			if (itemCodeCheck(item.getItemCode()) == 0) {
 				List<Bid> bidList = new ArrayList<>();
@@ -53,7 +81,13 @@ public class AuctionManager {
 			System.out.println("Adding "+bid);
 		}
 	}
-
+	/**
+	 * This function helps in identifying the bid winner and closing bidding on item
+	 * 
+	 * @param Item Object of item on which bidding is occurring
+	 * 
+	 * @return Bid Highest winner bid for the item
+	 * */
 	public Bid closeBiddingOnItem(Item item) {
 		if (bidManager.get(item) != null) {
 			Bid bid=bidManager.get(item).stream().max(Comparator.comparingInt(Bid::getBidPrice)).get();
@@ -62,23 +96,36 @@ public class AuctionManager {
 		}
 		return null;
 	}
-
+	/**
+	 * This function highest bid price for the item
+	 * @param Item for this highest bid has to be found out.
+	 * @return highest bid as integer
+	 * @throws IllegalStateException
+	 * */
 	public int getHighestBidPrice(Item item) {
 		if (bidManager.get(item) != null) {
 			return bidManager.get(item).stream().map(Bid::getBidPrice).mapToInt(v -> v).max().getAsInt();
 		}
 		throw new IllegalStateException();
 	}
-
+	/**
+	 * This function helps in finding there is no duplicate item code in bid manager.
+	 * @param itemCode code of item
+	 * @return 0 where there is no item with that particular code
+	 * */
 	public int itemCodeCheck(int itemCode) {
 		return bidManager.entrySet().stream().filter(p -> p.getKey().getItemCode() == itemCode).map(m -> m)
 				.collect(Collectors.counting()).intValue();
 	}
-
+	/**
+	 * This function list all item that are up for bidding
+	 * */
 	public void listAllItemForBid() {
 		bidManager.forEach((k, v) -> System.out.println("Listing "+k));
 	}
-
+	/**
+	 * This function list all bids for that item.
+	 * */
 	public void listAllBids(Item item) {
 		if (bidManager.get(item) != null) {
 			bidManager.get(item).stream().forEach(System.out::println);
--- src/main/java/com/pckage/auctionStore/core/Listner.java
+++ src/main/java/com/pckage/auctionStore/core/Listner.java
@@ -3,17 +3,38 @@ package com.pckage.auctionStore.core;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.pckage.auctionStore.action.AuctionManager;
-
+/**
+ * Listner class to monitor bid time restrictions to get highest bid winner
+ * 
+ * @author Abhishek Singh
+ * @version 1.0
+ */
 public class Listner {
+	/** 
+	 * To get the single instance of Listner class
+	 * */
 	private static Listner listner=new Listner();
+	/**
+	 * To store latest bid time for that item in time manager
+	 * */
 	private final ConcurrentHashMap<Item, Long> timeManager=new ConcurrentHashMap<>();
+	/**
+	 * 
+	 * */
 	private Listner(){
 		
 	}
+	/**
+	 * To get the single instance of listener class
+	 * */
 	public static Listner getInstance(){
 			return listner;
 	}
-	
+	/**
+	 * To notify any bid time update of the item
+	 * @param item Item on the bid
+	 * @param timeMilli latest time on last bid
+	 * */
 	public void notifyUpdate(Item item,Long timeInMilli){
 		System.out.println("Adding Notification....");
 		if(timeManager.get(item)==null){
@@ -22,6 +43,9 @@ public class Listner {
 			timeManager.replace(item, timeInMilli);
 		}
 	}
+	/**
+	 * To start listener thread
+	 * */
 	public void startListner(){
 		new Thread(()->{
 			System.out.println("Starting Listner....");
